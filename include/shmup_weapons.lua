ac100bbd091c90269342e1a45a7fa9cea58def9a
Alec Dutcher <aidutcher@yahoo.com>
2025-08-16 22:37:33 -0500

    Add bullet object and firing animation

--- include/shmup_weapons.lua
+++ include/shmup_weapons.lua
@@ -1,32 +1,28 @@
-function add_blt(x, y, dx, dy)
+function add_blt()
   if btn(5) then
 	  local bullet = {
+		sprite = 4,
 	    x = ship.x,
-	    y = ship.y-8,
-	    dx = dx,
-	    dy = dy,
-	    speed = 4,  -- example speed
-	    life = 60   -- example lifespan (frames)
+	    y = ship.y,
+	    speed = 8  -- example speed
 	  }
 	  add(blts, bullet)
 	 end
 end
 
 function upd_blts()
- for i = #bullets, 1, -1 do
-   local bullet = blts[i]
-   bullet.x += bullet.dx * bullet.speed
-   bullet.y += bullet.dy * bullet.speed
-   bullet.life -= 1
-   if bullet.life <= 0 then
-     del(bullets, i) -- remove the bullet if it has expired
+ for blt in all(blts) do
+   blt.y -= blt.speed
+   --remove bullet at
+   -- edge of screen
+   if blt.y >118 then
+     del(blts, blt) 
    end
  end
 end
 
 function draw_blts()
-	for i = #bullets, 1, -1 do
-		local bullet = blts[i]
-		spr(bullet.sprite, bullet.x, bullet.y)
+	for blt in all(blts) do
+		spr(blt.sprite, blt.x, blt.y)
 	end
 end
